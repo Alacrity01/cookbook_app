62d26c0cf4745bfaaa281c9918af1ff11c938b1a
TheAlacrity <brinker.jeff@gmail.com>
2019-05-06 10:21:23 -0500

    updated api

---
+++ app/controllers/api/recipes_controller.rb
@@ -0,0 +1,24 @@
+class Api::RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])    
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        prep_time: params[:prep_time],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions],
+                        image_url: params[:image_url]
+                        )
+    @recipe.save
+    render 'show.json.jbuilder'     <- also references show view
+  end
+end
