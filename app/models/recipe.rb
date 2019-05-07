76d8d7071adb178d961c4528bd77acc9d5772e44
TheAlacrity <brinker.jeff@gmail.com>
2019-05-07 11:12:05 -0500

    added recipe partial to DRY up show and index views

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,4 +10,23 @@ class Recipe < ApplicationRecord
   def friendly_created_at
     created_at.strftime("%b %d, %Y")
   end
+
+  def friendly_prep_time
+    # input: Integer prep_time 125
+    # output: String "2 hours, 5 minutes"
+
+    # 1 - Split time into hours and minutes
+    # 2 - create a english display of hours and minutes
+    hours = prep_time / 60
+    minutes = prep_time % 60
+
+    time_string = ""
+
+ 
+    time_string += "#{hours} #{'hour'.pluralize(hours)}" if hours > 0
+    time_string += ", " if hours > 0 && minutes > 0
+    time_string += "#{minutes} #{'minute'.pluralize(minutes)}" if minutes > 0
+  
+    time_string
+  end
 end
--- app/views/api/recipes/index.json.jbuilder
+++ app/views/api/recipes/index.json.jbuilder
@@ -1,10 +1,1 @@
-json.array! @recipes.each do |recipe|
-  json.id recipe.id
-  json.title recipe.title
-  json.chef recipe.chef
-  json.prep_time recipe.prep_time
-  json.ingredients recipe.ingredients_list
-  json.directions recipe.directions_list
-  json.image_url recipe.image_url
-  json.created_at recipe.friendly_created_at
-end
+json.array! @recipes, partial: "recipe", as: :recipe
