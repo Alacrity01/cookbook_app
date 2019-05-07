b2d75abcafd93a04fbb4c99e9a1240a66448e1d5
TheAlacrity <brinker.jeff@gmail.com>
2019-05-07 09:26:51 -0500

    added class methods for better formatting

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    self.ingredients.split(", ")
+  end
+
+  def directions_list
+    self.directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %d, %Y")
+  end
 end
