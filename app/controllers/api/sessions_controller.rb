5da38afda57ef99ae2f15a78f61ed56d2eba32b1
TheAlacrity <brinker.jeff@gmail.com>
2019-05-14 10:38:29 -0500

    fixed create action in sessions controller (pasted wrong code initially

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,17 +1,16 @@
 class Api::SessionsController < ApplicationController
-  def create
-    user = User.new(
-                    name: params[:name],
-                    email: params[:email],
-                    password: params[:password],
-                    password_confirmation: params[:password_confirmation]
-                    )
-
-    if user.save
-      render json: {message: 'User created successfully'}, status: :created
+ def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        'HS256' # the encryption algorithm
+      )
+      render json: {jwt: jwt, email: user.email, user_id: user.id}, status: :created
     else
-      render json: {errors: user.errors.full_messages}, status: :bad_request
-    end
-  end
-
+      render json: {}, status: :unauthorized
 end
